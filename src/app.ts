15b1bbbdda417259eccffe6306b30db6f3191b3d
jihun <jihunv8@gmail.com>
2023-02-23 11:55:32 +0900

    Feat cors설정 추가

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import express from 'express';
-import type { Express, Request, Response } from 'express';
+import cors from 'cors';
 import todoRouter from './routers/todosRouter';
 import usersRouter from './routers/usersRouter';
+
+import type { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(
+  cors({
+    // CORS 설정
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
+  })
+);
+
+app.use((req, res, next) => {
+  console.log('요청들어옴');
+  next();
+});
 
 app.use('/users', usersRouter);
 app.use('/todos', todoRouter);
